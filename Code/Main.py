52cc241f43cc22bcb14616ecfa07c76320d4b422
Kevin <kevingruizy@gmail.com>
2021-09-28 20:20:04 -0600

    Creacion Text y Scroll

--- Code/Main.py
+++ Code/Main.py
@@ -16,6 +16,41 @@ class Application():
         s.configure('TNotebook', tabposition='n',background='#17202a') 
         s.configure('TNotebook.Tab', font=('Segoe UI', 12,'bold'),background='#3498db', foreground='white', borderwidth=0)
         s.map('TNotebook.Tab', background=[('selected', '#2471a3'),('active', "#3498db")], foreground=[("selected", 'white')])
+        
+        labelframe = LabelFrame(self.root, text="Opciones",bg = "#2c3e50",bd=3,labelanchor='n',fg='white')
+        
+        labelframe.config(font=('Segoe UI', 20,'bold'))
+        labelframe.place(x=40, y=20,width=400,height=90)
+        
+        text2 = Text(self.root, height=20, width=50)
+        scroll = Scrollbar(self.root, command=text2.yview)
+        text2.configure(yscrollcommand=scroll.set)
+        text2.tag_configure('bold_italics', font=('Arial', 12, 'bold', 'italic'))
+        text2.tag_configure('big', font=('Verdana', 20, 'bold'))
+        text2.tag_configure('color',
+                    foreground='#476042',
+                    font=('Tempus Sans ITC', 12, 'bold'))
+        text2.tag_bind('follow',
+               '<1>',
+               lambda e, t=text2: t.insert(END, "Not now, maybe later!"))
+        text2.insert(END,'\nWilliam Shakespeare\n', 'big')
+        quote = """
+            To be, or not to be that is the question:
+            Whether 'tis Nobler in the mind to suffer
+            The Slings and Arrows of outrageous Fortune,
+            Or to take Arms against a Sea of troubles,
+            """
+        text2.insert(END, quote, 'color')
+        text2.insert(END, 'follow-up\n', 'follow')
+        text2.place(x=40, y=150,width=800,height=500)
+
+        scroll.pack(expand=True,padx=10, fill=Y)
+        scroll.place(x=823,y=150,height=500)
+        
+        
+        
+        
+        
         #s.theme_use("default")
         
     
